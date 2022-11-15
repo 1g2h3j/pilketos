356f80faf0b74360de727980ccb715e09cbec222
bagasimam12 <bagasimam01@gmail.com>
2022-11-15 22:19:48 +0700

    fix halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ ConfigVoting::getConfig()->title }} - {{ ConfigVoting::getConfig()->title_prefix }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('vendor/sweetalert2/sweetalert2.min.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -99,48 +102,56 @@
                 <div class="card">
                     <div class="card-body d-flex flex-column align-items-center justify-content-center py-4">
                         <h2><strong>Pemilihan Ketua & Wakil Ketua Osis</strong></h2>
-                        <img src="{{ asset('images/static/logosmk.png') }}" class="text-center py-3" alt="Logo" width="180">
+                        <img src="{{ asset('images/static/logosmk.png') }}" class="text-center py-3" alt="Logo"
+                            width="180">
                         <h2 class="mb-3"><strong> SMK Negeri 1 Bawang </strong></h2>
-                        
+
                         <form method="post" action="{{ route('login') }}">
                             @csrf
                             <div class="row">
                                 <div class="col-2 pt-2 d-flex flex-colummn">
-                                <div class="row d-flex flex-colummn gap-3">
-                                    <div class="btn btn-primary rounded-4" disabled><i class="fas fa-user pt"></i></div>
-                                    <div class="btn btn-primary rounded-4" disabled><i class="fas fa-key pt"></i></div>
-                                </div>
+                                    <div class="row d-flex flex-colummn gap-3">
+                                        <div class="btn btn-primary rounded-4" disabled><i class="fas fa-user pt"></i>
+                                        </div>
+                                        <div class="btn btn-primary rounded-4" disabled><i class="fas fa-key pt"></i>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="col-10">
-                            <div class="form-floating my-2">
-                                <input type="text" name="uuid" class="form-control rounded-4" autocomplete="off" id="inputNIS" value="{{ old('uuid') }}" placeholder="NIS" required="required">
-                                <label for="inputNIS">Masukkan NIS/NIP</label>
-                              </div>
-                              <div class="form-floating">
-                                <input type="text" name="token" class="form-control rounded-4" autocomplete="off" id="inputToken" value="" placeholder="Token" required="required">
-                                <label for="inputToken">Masukkan Token</label>
-                              </div>
+                                    <div class="form-floating my-2">
+                                        <input type="text" name="uuid" class="form-control rounded-4"
+                                            autocomplete="off" id="inputNIS" value="{{ old('uuid') }}"
+                                            placeholder="NIS" required="required">
+                                        <label for="inputNIS">Masukkan NIS/NIP</label>
+                                    </div>
+                                    <div class="form-floating">
+                                        <input type="text" name="token" class="form-control rounded-4"
+                                            autocomplete="off" id="inputToken" value="" placeholder="Token"
+                                            required="required">
+                                        <label for="inputToken">Masukkan Token</label>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
                             <div class="d-flex justify-content-center mt-4 login_container">
                                 <button type="submit" class="btn btn-primary rounded-4">MASUK</button>
                             </div>
                         </form>
-                            
+
                     </div>
-                  </div>
+                </div>
             </div>
         </div>
     </div>
 
     <style scoped>
-        body{
+        body {
             background-image: url("{{ asset('images/static/bg.jpg') }}");
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
         }
-        .card{
+
+        .card {
             background: rgba(255, 255, 255, 0.2);
             box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
             backdrop-filter: blur(8px);
@@ -148,9 +159,11 @@
             border-radius: 25px;
             border: none;
         }
-        .pt{
+
+        .pt {
             padding-top: 11px;
         }
+
         /* .form-control{
             width: 130%
         } */
@@ -163,21 +176,20 @@
     <script src="{{ asset('vendor/sweetalert2/sweetalert2.all.min.js') }}"></script>
 
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             @error('login')
-            Swal.fire({
-                icon: 'error',
-                title: 'Login Gagal!',
-                text: '{{ $message }}'
-            });
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Gagal!',
+                    text: '{{ $message }}'
+                });
             @enderror
 
-            @if(session() -> has('voting'))
-            Swal.fire({
-                icon: 'success',
-                text: '{{ session('
-                voting ') }}'
-            });
+            @if (session()->has('voting'))
+                Swal.fire({
+                    icon: 'success',
+                    text: '{{ session('voting') }}'
+                });
             @endif
         });
     </script>
