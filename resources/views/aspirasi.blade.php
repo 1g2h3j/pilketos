87309fb7887a2832fdbbe9e5a3eabd7ae5f3990d
1g2h3j <affannnmaull99@gmail.com>
2022-11-17 05:03:30 +0700

    Revert "fix halaman voting"
    This reverts commit bf4d7f577b4be0120d57e66c0b327d0712ba9d0b.

--- resources/views/aspirasi.blade.php
+++ resources/views/aspirasi.blade.php
@@ -11,52 +11,77 @@
             background-size: cover;
             font-family: 'Poppins', sans-serif;
         }
+
+        .wrapper {
+            height: 90%;
+            width: 85%;
+            position: absolute;
+            margin: auto;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+        }
+
+        .card {
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            background-color: rgba(255, 255, 255, 0.06);
+            border-radius: 10px;
+            box-shadow: 20px 20px 25px rgba(0, 0, 0, 0.2);
+            backdrop-filter: blur(10px);
+        }
     </style>
 @endsection
 
 @section('content')
 
-    <section class="form-aspirasi text-white">
+    <section class="form-aspirasi">
         <div class="container">
-            <form action="{{ route('aspirasi') }}" method="post">
-                @csrf
-                <input type="hidden" name="voting" value="{{ $voting }}">
-                <div class="col-md-12">
-                    <h1 class="text-center mt-2 mb-5"> <strong>Berikan Aspirasi Anda!</strong> </h1>
-                    <div class="card-body row">
-                        <div class="col-md-6">
-                            <p class="lead">Ketikan Ucapan Terima Kasih & Tanggapan untuk Pengurus OSIS Masa Bakti
-                                2021/2022
-                            </p>
-                            <div class="form-group has-feedback">
-                                <textarea name="ucapan" id="ucapan" cols="30" rows="10"
-                                    class="form-control  @error('ucapan') is-invalid @enderror" required=""></textarea>
-                                <div class="invalid-feedback">
-                                    @error('ucapan')
-                                        {{ $message }}
-                                    @enderror
+            <div class="wrapper">
+                <div class="card">
+                    <form action="{{ route('aspirasi') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="voting" value="{{ $voting }}">
+                        <div class="col-md-12">
+                            <h2 class="text-center mt-4">Berikan Aspirasi Anda!</h2>
+                            <div class="card-body row">
+                                <div class="col-md-6">
+                                    <p class="lead">Ketikan Ucapan Terima Kasih & Tanggapan untuk Pengurus OSIS Masa Bakti
+                                        2021/2022
+                                    </p>
+                                    <div class="form-group has-feedback">
+                                        <textarea name="ucapan" id="ucapan" cols="30" rows="10"
+                                            class="form-control  @error('ucapan') is-invalid @enderror" required=""></textarea>
+                                        <div class="invalid-feedback">
+                                            @error('ucapan')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="lead">Ketikan Saran & Aspirasi untuk Ketua & Wakil Ketua OSIS Masa Bakti
+                                        {{ ConfigVoting::getConfig()->periode }}</p>
+                                    <div class="form-group has-feedback">
+                                        <textarea name="aspirasi" id="aspirasi" cols="30" rows="10"
+                                            class="form-control  @error('aspirasi') is-invalid @enderror" required=""></textarea>
+                                        <div class="invalid-feedback">
+                                            @error('aspirasi')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <p class="lead">Ketikan Saran & Aspirasi untuk Ketua & Wakil Ketua OSIS Masa Bakti
-                                {{ ConfigVoting::getConfig()->periode }}</p>
-                            <div class="form-group has-feedback">
-                                <textarea name="aspirasi" id="aspirasi" cols="30" rows="10"
-                                    class="form-control  @error('aspirasi') is-invalid @enderror" required=""></textarea>
-                                <div class="invalid-feedback">
-                                    @error('aspirasi')
-                                        {{ $message }}
-                                    @enderror
-                                </div>
+                            <div class="col-sm-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-block">Kirim Aspirasi</button>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-sm-12 mt-4 text-center">
-                        <button type="submit" class="btn btn-primary btn-block">Kirim Aspirasi</button>
-                    </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </section>
 
@@ -67,7 +92,7 @@
     <script>
         $(document).ready(function() {
             @error('voting')
-                swal({
+                Swal.fire({
                     icon: 'error',
                     title: 'Error!',
                     text: '{{ $message }}'
@@ -75,7 +100,7 @@
             @enderror
 
             @if (session()->has('voting'))
-                swal({
+                Swal.fire({
                     icon: 'success',
                     text: '{{ session('voting') }}'
                 });
--- resources/views/voting_osis.blade.php
+++ resources/views/voting_osis.blade.php
@@ -1,170 +1,276 @@
 @extends('layouts.default')
 
-@section('css')
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" !important>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+@section('headscript')
+@endsection
+@section('content')
+{{-- <nav class="navbar navbar-expand-lg fixed-top navbar-dark" style="background-color: gray;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">KANDIDAT CALON  OSIS {{ ConfigVoting::getConfig()->periode }}</a>
+</div>
+</nav>
 
-    <style scoped>
-        .no-paslon {
-            position: absolute;
-            /* padding: 30px 15px 8px 30px; */
-            padding: 65px 25px 15px 76px;
-            border-radius: 50%;
-            font-size: 1.5rem;
-            color: white;
-            background-color: #0d6dfc;
-            left: -60px;
-            top: -55px;
-            box-shadow: 0 8px 32px 0 rgba(6, 6, 6, 0.3);
-        }
+<div style="margin-bottom: 30px; margin-top: 110px;"></div>
 
-        .card {
-            border: none !important;
-        }
+<ul class="cards">
+  @foreach($kandidat as $row)
+  <li>
+    <a href="#kandidat-{{ $row->paslon_no }}" class="card">
+      <img src="{{ asset($row->gambar) }}" class="card__image pb-5" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+            <path /></svg>
+          <img class="card__thumb" src="{{ asset('images/static/icon/number' . $row->paslon_no . '.png') }}" alt="" />
+          <div class="card__header-text">
+            <h3 class="card__title">{{ $row->ketua }} & {{ $row->wakil }}</h3>
+            <button data-toggle="modal" data-target="#modalVisiMisi" data-visi="{!! $row->visi !!}"
+              data-misi="{!! makeList($row->misi, '|') !!}" type="button" class="btn btn-info">VISI & MISI</button>
+            <button data-toggle="modal" data-target="#modalVoteKandidat" data-ketua="{{ $row->ketua }}"
+              data-wakil="{{ $row->wakil }}" data-kandidat="{{ $row->id }}" type="button"
+              class="btn btn-primary">PILIH</button>
+          </div>
+        </div>
+      </div>
+    </a>
+  </li>
+  @endforeach
+</ul>
 
-        .card {
-            color: black;
-            background: rgba(200, 200, 200, 0.37);
-            box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-            backdrop-filter: blur(4px);
-            -webkit-backdrop-filter: blur(4px);
-            border-radius: 10px;
-            border: 1px solid rgba(255, 255, 255, 0.18);
-        }
+<div class="modal fade" id="modalVoteKandidat" data-backdrop="static" data-keyboard="false" tabindex="-1"
+  aria-labelledby="modalVoteKandidatLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalVoteKandidatLabel">Konfirmasi Pemilihan</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p class="lead">Dengan ini dengan penuh kesadaran dan dengan tanpa paksaan saya memilih <b><span
+              id="paslon"></span></b> sebagai calon ketua dan wakil ketua OSIS
+          {{ ConfigVoting::getConfig()->title_prefix }} untuk periode {{ ConfigVoting::getConfig()->periode }}.</p>
+      </div>
+      <div class="modal-footer">
+        <form action="{{ route('vote.osis') }}" method="post">
+          @csrf
+          <input type="hidden" id="kandidat-id" name="kandidat_id">
+          <button type="submit" class="btn btn-primary">Konfirmasi Pemilihan</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
-        .card .card-paslon {
-            box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.05);
-        }
+<div class="modal fade" id="modalVisiMisi" data-backdrop="static" data-keyboard="false" tabindex="-1"
+  aria-labelledby="modalVisiMisiLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalVisiMisiLabel">Visi Misi Kandidat</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-12">
+            <h6 class="text-center">Visi</h6>
+            <hr>
+            <p id="visi"></p>
+          </div>
+          <div class="col-md-12">
+            <h6 class="text-center">Misi</h6>
+            <hr>
+            <div id="misi"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-        .modal-body p {
-            margin-bottom: 0.5rem;
-            /* letter-spacing: 0.5px; */
-            font-weight: 200 !important;
-        }
-    </style>
+<div class="footer-basic">
+  <footer>
+    <p class="copyright">
+      <b>{{ strtoupper(ConfigVoting::getConfig()->title) }} <br>
+        {{ strtoupper(ConfigVoting::getConfig()->title_prefix) }}</b>
+    </p>
+  </footer>
+</div> --}}
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <!-- card-paslon -->
+  @foreach($kandidat as $row)
+  <div class="col d-flex justify-content-center">
+    <div class="card h-100 card-paslon d-flex align-items-center text-center" style="width: 18rem">
+      <div class="no-paslon">{{ $row->paslon_no }}</div>
+      <img src="{{ asset($row->gambar) }}" alt="photo-profile" width="120" class="pt-4 rounded-circle" />
+      <div class="card-body">
+        <h5 class="card-title fst-italic m-0">Ketua</h5>
+        <p class="card-text fw-bold">{{ $row->ketua }}</p>
+        <h5 class="card-title fst-italic m-0">Wakil Ketua</h5>
+        <p class="card-text fw-bold">{{ $row->wakil }}</p>
+        <div class="row gap-2 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary col-5" data-bs-toggle="modal" data-bs-target="#visi"
+            data-bs-visi="{!! $row->visi !!}">VISI</button>
+          <button type="button" class="btn btn-primary col-5" data-bs-toggle="modal"
+            data-bs-target="#misi">MISI</button>
+          <button data-bs-toggle="modal" data-bs-target="#modalVoteKandidat" data-bs-ketua="{{ $row->ketua }}"
+            data-bs-wakil="{{ $row->wakil }}" data-bs-kandidat="{{ $row->id }}" type="button"
+            class="btn btn-primary">PILIH</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--end card-paslon -->
+  @endforeach
+</div>
+
+<div class="modal fade" id="modalVoteKandidat" tabindex="-1" aria-labelledby="modalVoteKandidatLabel"
+  aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalVoteKandidatLabel">Konfirmasi Pemilihan</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body p-4 fst-italic" <p class="lead">Dengan ini dengan penuh kesadaran dan dengan tanpa paksaan
+        saya memilih <b><span id="paslon"></span></b> sebagai calon ketua dan wakil ketua OSIS
+        {{ ConfigVoting::getConfig()->title_prefix }} untuk periode {{ ConfigVoting::getConfig()->periode }}.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+          <form action="{{ route('vote.osis') }}" method="post">
+            @csrf
+            <input type="hidden" id="kandidat-id" name="kandidat_id">
+            <button type="submit" class="btn btn-primary">Konfirmasi Pemilihan</button>
+          </form>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+@foreach($kandidat as $row)
+<!-- modal visi -->
+<div class="modal fade" id="visi" tabindex="-1" aria-labelledby="visiLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="visiLabel"><strong>VISI</strong></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body p-4 fst-italic">
+        <p>{{ $row->visi }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!--end modal visi -->
+
+<!-- modal misi -->
+<div class="modal fade" id="misi" tabindex="-1" aria-labelledby="misiLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="misiLabel"><strong>MISI</strong></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body p-4 fst-italic">{!! $row->misi !!}
+      </div>
+
+    </div>
+  </div>
+</div>
+<!--end modal misi -->
+@endforeach
+<style scoped>
+  .no-paslon {
+    position: absolute;
+    /* padding: 30px 15px 8px 30px; */
+    padding: 65px 25px 15px 76px;
+    border-radius: 50%;
+    font-size: 1.5rem;
+    color: white;
+    background-color: #0d6dfc;
+    left: -60px;
+    top: -55px;
+    box-shadow: 0 8px 32px 0 rgba(6, 6, 6, 0.3);
+  }
+
+  .card {
+    border: none !important;
+  }
+
+  .card {
+    color: whitesmoke;
+    background: rgba(200, 200, 200, 0.37);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(4px);
+    -webkit-backdrop-filter: blur(4px);
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+
+  .card .card-paslon {
+    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.05);
+  }
+
+  .modal-body p {
+    margin-bottom: 0.5rem;
+    /* letter-spacing: 0.5px; */
+    font-weight: 200 !important;
+  }
+</style>
 @endsection
 
-@section('content')
+@push('footscript')
+<script>
+  @error('voting')
+  swal({
+    icon: 'error',
+    title: 'Error!',
+    text: '{{ $message }}'
+  });
+  @enderror
 
-    <ul class="cards">
-        @foreach ($kandidat as $row)
-            <li>
-                <a href="#kandidat-{{ $row->paslon_no }}"
-                    class="card h-100 card-paslon d-flex align-items-center text-center" style="width: 18rem">
-                    <div class="no-paslon">{{ $row->paslon_no }}</div>
-                    <img src="{{ asset($row->gambar) }}" alt="photo-profile" width="120" class="pt-4 rounded-circle" />
-                    <div class="card-body">
-                        <h5 class="card-title fst-italic m-0">Ketua</h5>
-                        <p class="card-text fw-bold">{{ $row->ketua }}</p>
-                        <h5 class="card-title fst-italic m-0">Wakil Ketua</h5>
-                        <p class="card-text fw-bold">{{ $row->wakil }}</p>
-                        <div class="row gap-2 d-flex justify-content-center">
-                            <button type="button" class="open_visi btn btn-primary col-5" data-toggle="modal"
-                                data-target="#modalVisi" data-visi="{!! $row->visi !!}">VISI</button>
-                            <button type="button" class="btn btn-primary col-5" data-toggle="modal"
-                                data-target="#modalMisi" data-misi="{!! $row->visi !!}">MISI</button>
-                            <button data-toggle="modal" data-target="#modalVoteKandidat" data-ketua="{{ $row->ketua }}"
-                                data-wakil="{{ $row->wakil }}" data-kandidat="{{ $row->id }}" type="button"
-                                class="btn btn-primary">PILIH</button>
-                        </div>
-                    </div>
-                </a>
-            </li>
-        @endforeach
-    </ul>
+  @if(session() -> has('message'))
+  swal({
+    icon: 'info',
+    title: 'Welcome',
+    text: '{{ session("message") }}'
+  });
+  @endif
 
-    <div class="modal fade" id="modalVoteKandidat" data-backdrop="static" data-keyboard="false" tabindex="-1"
-        aria-labelledby="modalVoteKandidatLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalVoteKandidatLabel">Konfirmasi Pemilihan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p class="lead">Dengan ini dengan penuh kesadaran dan dengan tanpa paksaan saya memilih <b><span
-                                id="paslon"></span></b> sebagai calon ketua dan wakil ketua OSIS
-                        {{ ConfigVoting::getConfig()->title_prefix }} untuk periode
-                        {{ ConfigVoting::getConfig()->periode }}.</p>
-                </div>
-                <div class="modal-footer">
-                    <form action="{{ route('vote.osis') }}" method="post">
-                        @csrf
-                        <input type="hidden" id="kandidat-id" name="kandidat_id">
-                        <button type="submit" class="btn btn-primary">Konfirmasi Pemilihan</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+  @if(session() -> has('welcome'))
+  swal({
+    icon: 'success',
+    text: 'Selamat memilih!'
+  });
+  @endif
+  $(document).ready(function () {
+    $('#modalVisiMisi').on('show.bs.modal', function (e) {
+      var modal = $(this);
+      var button = $(e.relatedTarget);
 
-    <div class="modal fade" id="modalVisi" data-backdrop="static" data-keyboard="false" tabindex="-1"
-        aria-labelledby="modalVisiLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalVisiLabel">VISI</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h6 class="text-center">Visi</h6>
-                            <hr>
-                            <p id="visi"></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+      modal.find('#visi').text(button.data('visi'));
+      modal.find('#misi').html(button.data('misi'));
+    });
 
-    <div class="modal fade" id="modalMisi" data-backdrop="static" data-keyboard="false" tabindex="-1"
-        aria-labelledby="modalMisiLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalMisiLabel">MISI</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h6 class="text-center">MISI</h6>
-                            <hr>
-                            <div id="misi"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    {{-- javascript --}}
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.10/dist/sweetalert2.all.min.js"></script>
-    {{-- end js --}}
-@stop
+    $('#modalVisiMisi').on('hide.bs.modal', function (e) {
+      var modal = $(this);
+      var button = $(e.relatedTarget);
 
-@push('js')
-    <script>
-        $(document).ready(function() {
-            $('#modalVisi').on('show.bs.modal', function(e) {
-                var modal = $(this);
-                var button = $(e.relatedTarget);
+      modal.find('#visi').text('');
+      modal.find('#misi').html('');
+    });
 
-                modal.find('#visi').text(button.data('visi'));
-            });
+    $('#modalVoteKandidat').on('show.bs.modal', function (e) {
+      var modal = $(this);
+      var button = $(e.relatedTarget);
 
+<<<<<<< HEAD
 <!-- modal misi -->
 <div class="modal fade" id="misi" tabindex="-1" aria-labelledby="misiLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg modal-dialog-centered">
@@ -194,11 +300,29 @@
     top: -55px;
     box-shadow: 0 8px 32px 0 rgba(6, 6, 6, 0.3);
   }
+=======
+      modal.find('#kandidat-id').val(button.data('kandidat'));
+      modal.find('#paslon').text(button.data('ketua') + ' & ' + button.data('wakil'));
+    });
 
-            $('#modalMisi').on('show.bs.modal', function(e) {
-                var modal = $(this);
-                var button = $(e.relatedTarget);
+    @error('voting')
+    Swal.fire({
+      icon: 'error',
+      title: 'Error!',
+      text: '{{ $message }}'
+    });
+    @enderror
+>>>>>>> parent of bf4d7f5 (fix halaman voting)
 
+    @if(session() -> has('message'))
+    Swal.fire({
+      icon: 'info',
+      title: 'Welcome',
+      text: '{{ session("message") }}'
+    });
+    @endif
+
+<<<<<<< HEAD
                 modal.find('#misi').html(button.data('misi'));
             });
 
@@ -273,10 +397,18 @@
 
     @if(session() -> has('welcome'))
     swal({
+=======
+    @if(session() -> has('welcome'))
+    Swal.fire({
+>>>>>>> parent of bf4d7f5 (fix halaman voting)
       icon: 'success',
       text: 'Selamat memilih!'
     });
     @endif
   });
 </script>
+<<<<<<< HEAD
 @endpush
+=======
+@endpush
+>>>>>>> parent of bf4d7f5 (fix halaman voting)
